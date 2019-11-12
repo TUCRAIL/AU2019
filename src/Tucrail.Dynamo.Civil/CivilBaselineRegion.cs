63553b87d50516570dd7e10d320399c7b70704d2
Steve CROKAERT <Steve.CROKAERT@tucrail.be>
2019-11-12 10:45:54 +0100

    Initial commit

---
+++ src/Tucrail.Dynamo.Civil/CivilBaselineRegion.cs
@@ -0,0 +1,26 @@
+﻿using Autodesk.Civil.DynamoNodes;
+using Dynamo.Graph.Nodes;
+using Corridor = Autodesk.Civil.DatabaseServices.Corridor;
+
+
+public class CivilBaselineRegion
+{
+    private CivilBaselineRegion()
+    {
+    }
+
+    /// <summary>
+    /// Create a baseline region based on an asset
+    /// </summary>
+    public static int CreateFromAsset(Baseline baseline, string assetName, string assemblyName, double assetStartStation, double assetEndStation)
+    {
+        if (string.IsNullOrEmpty(assetName)) return -1;
+        if (string.IsNullOrEmpty(assemblyName)) return -1;
+        if (baseline == null) return -1;
+
+        var corridor = (Corridor)baseline.Corridor.InternalDBObject;
+        var baseBaseline = corridor.Baselines[baseline.Name];
+        baseBaseline.BaselineRegions.Add(assetName, assemblyName, assetStartStation, assetEndStation);
+        return baseBaseline.BaselineRegions.IndexOf(baseBaseline.BaselineRegions[assetName]);
+    }
+}
